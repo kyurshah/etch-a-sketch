6f387ab79cdb6e51a92c96bca480566f6af094fc
keyur <13107634+kyurshah@users.noreply.github.com>
2024-12-11 13:17:00 +0530

    Refactored the entire codebase

--- script.js
+++ script.js
@@ -1,35 +1,61 @@
 console.log("this is working");
 
-const container = document.querySelector("#container");
+const gridContainer = document.querySelector("#gridContainer");
 const resetButton = document.querySelector("#resetButton");
+const setGridSize = document.querySelector("#gridSizeButton");
+const eraser = document.querySelector("#eraserButton");
 
-let square = 16; 
-let totalSquare = square * square; 
-let color = "yellow"
-let initialGridColor = "rgb(209, 204, 204)"
+let square = 16;
+let totalSquares = square * square;
 
 
-const madeGrid = function() {
-    container.innerHTML = ""
-    for (let i = 0 ; i < totalSquare ; i++) {
-        const grid = document.createElement("div");
-        const gridSize = 100 / square;
-    
-        grid.innerText = ""
-        grid.className = "grid"
-        grid.style.backgroundColor = initialGridColor;
-        container.appendChild(grid);
-        grid.addEventListener("mouseenter", function () {
-            grid.style.backgroundColor = color;
-        })
-        grid.style.width = `${gridSize}%`;
-        grid.style.height = `${gridSize}%`;
+function squareProperty(grid) {
+    let gridSize = 100 / square ;
+
+    grid.innertext = "";
+    grid.className = "square";
+    grid.style.height = `${gridSize}%`;
+    grid.style.width = `${gridSize}%`;
+    grid.style.backgroundColor = "rgb(240, 240, 240)";
+    grid.style.margin = 0;
+    grid.style.padding = 0;
+    grid.style.opacity = 0.05;
+}
+
+function rgbColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random ()* 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+
+}
+
+function mouseHoverEffect(grid) {
+    grid.addEventListener("mouseover", function() {
+        grid.style.backgroundColor = rgbColor();
+
+        let currentOpacity = parseFloat(grid.style.opacity);
+        if (currentOpacity < 1) {
+          grid.style.opacity = (currentOpacity + 0.1).toFixed(1);
+        }
+    })
+
+}
+
+function createGrid() {
+
+    for (let i = 0 ; i < totalSquares ; i++) {
+        const grid = document.createElement("button");
+
+        squareProperty(grid);
+        gridContainer.appendChild(grid);
+
+        mouseHoverEffect(grid);
+
+
     }
 }
 
-madeGrid()
+createGrid()
 
-
-resetButton.addEventListener("click", function() {
-    madeGrid()
-})
